e7e0dcbf73fe7a28eac5a192785138aac9b949fd
ariansargazi <163904443+ariansargazi@users.noreply.github.com>
2024-03-22 19:37:32 -0400

    Update pf.py

--- pf.py
+++ pf.py
@@ -1,18 +1,23 @@
 import numpy as np
+
 from env import Field
+
 
 class ParticleFilter:
     def __init__(self, mean, cov, num_particles, alphas, beta):
         self.alphas = alphas
         self.beta = beta
+
         self._init_mean = mean
         self._init_cov = cov
         self.num_particles = num_particles
         self.reset()
 
     def reset(self):
-        self.particles = np.random.multivariate_normal(
-            self._init_mean.ravel(), self._init_cov, self.num_particles)
+        self.particles = np.zeros((self.num_particles, 3))
+        for i in range(self.num_particles):
+            self.particles[i, :] = np.random.multivariate_normal(
+                self._init_mean.ravel(), self._init_cov)
         self.weights = np.ones(self.num_particles) / self.num_particles
 
     def move_particles(self, env, u):
@@ -54,14 +59,21 @@ class ParticleFilter:
         return particles[indexes, :]
 
     def mean_and_variance(self, particles):
-        """Compute the mean and covariance matrix for a set of equally-weighted particles."""
-        mean = np.average(particles, weights=self.weights, axis=0)
+        """Compute the mean and covariance matrix for a set of equally-weighted
+        particles.
+
+        particles: (n x 3) matrix of poses
+        """
+        mean = particles.mean(axis=0)
         mean[2] = np.arctan2(
-            np.sum(np.sin(particles[:, 2])*self.weights),
-            np.sum(np.cos(particles[:, 2])*self.weights)
+            np.sin(particles[:, 2]).sum(),
+            np.cos(particles[:, 2]).sum(),
         )
-        zero_mean_particles = particles - mean
-        for i in range(len(zero_mean_particles)):
-            zero_mean_particles[i, 2] = Field.minimized_angle(zero_mean_particles[i, 2])
-        cov = np.cov(zero_mean_particles.T, aweights=self.weights)
+
+        zero_mean = particles - mean
+        for i in range(zero_mean.shape[0]):
+            zero_mean[i, 2] = Field.minimized_angle(zero_mean[i, 2])
+        cov = np.dot(zero_mean.T, zero_mean) / self.num_particles
+        cov += np.eye(particles.shape[1]) * 1e-6  # Avoid bad conditioning
+
         return mean.reshape((-1, 1)), cov
